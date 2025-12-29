5a691cd8900e7ec26b1e870f069dc4a83f502b26
unfaiyted <unfaiyted@gmail.com>
2025-12-28 19:59:35 -0600

    fix(keymap): replace GUI tap-hold with tilde in directions layer
    Change A key position from LGUI_T(KC_A) to KC_TILDE for direct
    tilde access in the directions layer.

--- keyboards/drop/cstm65/keymaps/faiyt/keymap.c
+++ keyboards/drop/cstm65/keymaps/faiyt/keymap.c
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DIRECTIONS] = LAYOUT_65_ansi_blocker(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        KC_GRV,  LGUI_T(KC_A),    LALT_T(KC_S),    MT(MOD_LCTL, KC_D),    LSFT_T(KC_F),    KC_G,    KC_LEFT,    KC_DOWN,   KC_UP, KC_RIGHT, KC_SCLN, KC_GRV, KC_ENT,  KC_PGUP,
+        KC_GRV,  KC_TILDE,    LALT_T(KC_S),    MT(MOD_LCTL, KC_D),    LSFT_T(KC_F),    KC_G,    KC_LEFT,    KC_DOWN,   KC_UP, KC_RIGHT, KC_SCLN, KC_GRV, KC_ENT,  KC_PGUP,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, _______,   KC_LEFT, KC_DOWN, KC_RGHT
     ),
